0c393b284f4ab65ae380ba002f7ed75ebcfb06a8
Kenneth Kay <kennethk6806@protonmail.com>
2025-04-22 22:19:43 -0400

    Working Item mod loads initializes and does something

---
+++ src/main/java/com/aerogames/regiments1core/RegimentS1CoreDataGenerator.java
@@ -0,0 +1,11 @@
+package com.aerogames.regiments1core;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class RegimentS1CoreDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
---
+++ src/main/java/com/aerogames/regiments1core/item/ModItems.java
@@ -0,0 +1,27 @@
+package com.aerogames.regiments1core.item;
+
+import com.aerogames.regiments1core.RegimentS1Core;
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroupEntries;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
+
+public class ModItems {
+    public static final Item OGBUSH = registerItem("ogbush", new Item(new FabricItemSettings()));
+
+    private static void addItemsToIngrediantItemGroup(FabricItemGroupEntries entries) {
+        entries.add(OGBUSH);
+    }
+    private static Item registerItem(String name, Item item) {
+        return Registry.register(Registries.ITEM, new Identifier(RegimentS1Core.MOD_ID, name), item);
+    }
+
+    public static void registerModItems(){
+    RegimentS1Core.LOGGER.info("Registering Mod Items for " + RegimentS1Core.MOD_ID);
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(ModItems::addItemsToIngrediantItemGroup);
+    }
+}
