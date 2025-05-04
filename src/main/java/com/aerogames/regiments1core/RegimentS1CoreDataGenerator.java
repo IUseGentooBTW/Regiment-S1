b32fa7f65761148e52d7d7ff944e6f81cb46a141
Kenneth Kay <kennethk6806@protonmail.com>
2025-05-04 14:23:45 -0400

    Datagen Works IDK where it goes lol

--- src/main/java/com/aerogames/regiments1core/RegimentS1CoreDataGenerator.java
+++ src/main/java/com/aerogames/regiments1core/RegimentS1CoreDataGenerator.java
@@ -1,11 +1,18 @@
 package com.aerogames.regiments1core;
 
+import com.aerogames.regiments1core.datagen.*;
 import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
 
 public class RegimentS1CoreDataGenerator implements DataGeneratorEntrypoint {
 	@Override
 	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+		FabricDataGenerator.Pack pack = fabricDataGenerator.createPack();
 
+		pack.addProvider(ModBlockTagProvider::new);
+		pack.addProvider(ModItemTagProvider::new);
+		pack.addProvider(ModLootTableProvider::new);
+		pack.addProvider(ModModelProvider::new);
+		pack.addProvider(ModRecipieProvider::new);
 	}
 }
--- src/main/java/com/aerogames/regiments1core/datagen/ModBlockTagProvider.java
+++ src/main/java/com/aerogames/regiments1core/datagen/ModBlockTagProvider.java
@@ -1,8 +1,10 @@
 package com.aerogames.regiments1core.datagen;
 
+import com.aerogames.regiments1core.block.ModBlocks;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricTagProvider;
 import net.minecraft.registry.RegistryWrapper;
+import net.minecraft.registry.tag.BlockTags;
 
 import java.util.concurrent.CompletableFuture;
 
@@ -15,6 +17,11 @@ public class ModBlockTagProvider extends FabricTagProvider.BlockTagProvider {
 
     @Override
     protected void configure(RegistryWrapper.WrapperLookup arg) {
-
+        getOrCreateTagBuilder(BlockTags.AXE_MINEABLE)
+                .add(ModBlocks.STORAGESHELF)
+                .add(ModBlocks.WOODENTABLE);
+        getOrCreateTagBuilder(BlockTags.PICKAXE_MINEABLE)
+                //.add(ModBlock.PLACEHOLDER);
+        ;
     }
 }
